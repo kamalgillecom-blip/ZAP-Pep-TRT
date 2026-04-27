f7e47e90762e6104492a10dfb3e62c7ddbdc6f87
Kamal Gill <kamalgillecom@gmail.com>
2026-04-26 22:38:29 -0400

    Move Firebase config to env vars, add netlify.toml, fix landing page button links

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCt8CyPWdI5O9sDOhU7AxmsQnnnVbulmcQ',
-  authDomain: 'peptide-c7e91.firebaseapp.com',
-  projectId: 'peptide-c7e91',
-  storageBucket: 'peptide-c7e91.appspot.com',
-  messagingSenderId: '934474605352',
-  appId: '1:934474605352:web:webapp',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
